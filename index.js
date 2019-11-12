c678c2d2a737c109d11f61c4180e6ba9ef75094a
Thomas Miller <tom.a.miller@gmail.com>
2019-11-12 10:59:19 -0500

    comment updated

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const server = require('http').Server(app);
 var io = require('socket.io')(server);
 const dotenv = require('dotenv');
 
-// http://expressjs.com/en/starter/static-files.html
+// load public/index.html
 app.use(express.static('public'));
+
+// load .env file for configuration
 dotenv.config();
 
+// define global variables
 var stat = {
   total_tweets: 0,
   tweets_per_sec: 0,
@@ -24,30 +27,42 @@ var g_domains = {};
 var g_tweets_emoji = 0;
 var g_emojis = {};
 
+
+// set callback for new websocket connection
 io.on('connection', function(socket) {
 
+    // Connect with Twitter stream with 'i' characters. 
+    // With this 'i', we can get almost 50 tweets per sec. if better option, we can use them.
     var stream = T.stream('statuses/filter', { track: 'i', language: 'en' })
 
     stream.on('tweet', function (tweet) {
+        // increase total tweets
         stat.total_tweets += 1;
 
         // add tweet to list
         g_tweets_arry.push(tweet);
     });
 
+    // send statistic infos to client
     setInterval(function() {
+
+      // calc total second from app start
       total_sec += 1;
+
       stat.tweets_per_sec = (stat.total_tweets/(total_sec));
 
+      // reverse sort for hashtag, get top 3 hashtags
       var sortable = [];
       for( var tag in g_hashtags) {
         sortable.push({tag: tag, value: g_hashtags[tag]});
       }
+      
       sortable.sort((a, b) => {
         return b.value - a.value;
       })
       stat["top_hashtag"] = sortable.slice(0, 3);
 
+      // reverse sort for domains, get top 3 domains 
       sortable = [];
       for( var domain in g_domains) {
         sortable.push({domain: domain, value: g_domains[domain]});
@@ -58,6 +73,7 @@ io.on('connection', function(socket) {
       });
       stat["tweets_domain"] = sortable.slice(0, 3);
 
+      // reverse sort for emoji list, get top 3 emojis
       sortable = [];
       for( var emoji in g_emojis) {
         sortable.push({emoji: emoji, value: g_emojis[emoji]});
@@ -72,12 +88,14 @@ io.on('connection', function(socket) {
       stat["tweets_photo"] = g_tweets_photo;
       stat["tweets_emoji"] = g_tweets_emoji;
       
+      // emit json data 
       io.emit("status", stat);
     }, 1000);
 
     extractInfos();
 });
 
+// get emoji and push to global array
 function emojiStringToArray (str) {
   split = str.split(/([\uD800-\uDBFF][\uDC00-\uDFFF])/);
   arr = [];
@@ -89,6 +107,8 @@ function emojiStringToArray (str) {
   }
   return arr;
 };
+
+// parse first tweet and extract infos
 function extractInfos() {
   if(g_tweets_arry.length > 0) {
     var tweet = g_tweets_arry[0];
@@ -96,7 +116,6 @@ function extractInfos() {
     // parse hashtags
     if(tweet.entities.hashtags.length > 0) {
       tweet.entities.hashtags.forEach(tag => {
-        // console.log(tag);
         if(g_hashtags[tag.text] == undefined) {
           g_hashtags[tag.text] = 1;
         }
@@ -127,7 +146,6 @@ function extractInfos() {
         }
       });
     }
-
 
     //parse tweets that contains photo
     if(tweet.entities.media != undefined && tweet.entities.media.length > 0) {
@@ -165,10 +183,10 @@ function extractInfos() {
 
     g_tweets_arry.shift();
   }
-  // console.log(g_tweets_arry.length);
   setImmediate(extractInfos);
 }
 
+// create Twitter stream object
 var T = new Twit({
   consumer_key:         process.env.CONSUMER_KEY,
   consumer_secret:      process.env.CONSUMER_SECRET,
